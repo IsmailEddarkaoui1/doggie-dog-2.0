8d08a234a5752f4ed32eab47078df129754cc61b
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-12-28 14:00:40 +0100

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
-        <Layout pageTitle="Welcome to the Dog asylum!">
-            <p>Buy your dog here. For only 20.99 euros.</p>
-        </Layout>
+        <main>
+            <Layout pageTitle="Welcome to the Dog asylum!">
+                <p>Buy your dog here. For only 20.99 euros.</p>
+                <StaticImage alt="My new dogs" src="../images/dogs.png"/>
+            </Layout>
+        </main>
     )
 }
+
 export default IndexPage
