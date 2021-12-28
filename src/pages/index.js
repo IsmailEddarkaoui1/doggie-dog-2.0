a0071057281b7c4e0d6a8dbeec044e088ff44b4e
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-12-28 13:46:28 +0100

    test

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+    return (
+        <Layout pageTitle="Welcome to the Dog asylum!">
+            <p>Buy your dog here. For only 20.99 euros.</p>
+        </Layout>
+    )
+}
+export default IndexPage
