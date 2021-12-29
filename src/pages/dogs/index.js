e561bd48b3f4ae988079c34f22d709e88b63c5b8
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-12-29 12:57:33 +0100

    Artists page: opmaak en styling

--- src/pages/dogs/index.js
+++ src/pages/dogs/index.js
@@ -1,57 +1,109 @@
 import * as React from 'react'
-import {Link, graphql} from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../../components/layout'
+import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+import Dog from "../../components/dog"
+import {
+    hero,
+    section,
+    subtitle,
+    artists,
+    description
+} from "../../page.module.css"
+
 
 const DogsPage = ({
     data: {
         allWpDog: {
-            edges
+            edges: dogsInfo
+        },
+        wpPage: {
+            dogsPageFields
         }
     }
 }) => {
+    const image = getImage(dogsPageFields.headerDogs.picture.localFile)
+
     return (
-        <Layout pageTitle="Dogs of Doggie-dog Agency">
-            {
-            edges.map((item) => {
-                const dog = item.node.dogFields;
-                const slug = item.node.slug;
-                return <Link to={
-                    `/dogs/${slug}`
-                }>
-                    <p key={
-                        item.node.id
-                    }>
-                        {
-                        dog.name
-                    } </p>
-                </Link>
-        })
-        } </Layout>
+        <Layout pageTitle="Dogs of doggie-dog Agency">
+            <GatsbyImage className={hero}
+                image={image}
+                alt={
+                    dogsPageFields.headerDogs.picture.altText
+                }/>
+            <div className={section}>
+                <h2 className={subtitle}>
+                    {
+                    dogsPageFields.headerDogs.title
+                }</h2>
+                <div className={description}
+                    dangerouslySetInnerHTML={
+                        {__html: dogsPageFields.headerDogs.description}
+                    }/>
+                <div className={artists}>
+                    {
+                    dogsInfo.map(({node: dog}) => (
+                        <Dog key={
+                                dog.id
+                            }
+                            slug={
+                                dog.slug
+                            }
+                            dog={dog}/>
+                    ))
+                } </div>
+            </div>
+        </Layout>
     )
 }
 
 export const query = graphql `
-query{
-    allWpDog {
-      edges {
-        node {
-          id
-          slug
-          dogFields {
-            name
-            age
-            sex
-            price
-            wormed
-            sterilized
-            chieldFriendly
-            description
+query {
+  wpPage(slug: {eq: "dogs"}) {
+    dogsPageFields {
+      headerDogs {
+        description
+        title
+        picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+            }
           }
+          altText
         }
       }
     }
   }
+  allWpDog {
+    edges {
+      node {
+        dogFields {
+          name
+          age
+          sex
+          price
+          wormed
+          sterilized
+          chieldFriendly
+          description
+          images {
+            picture1 {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: false})
+                }
+              }
+              altText
+            }
+          }
+        }
+        slug
+        id
+      }
+    }
+  }
+}
 
 `
-
 export default DogsPage
