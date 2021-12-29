a75043ce4e2c59ea1bc5fe26c06c85d3c3badcb9
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-12-29 14:48:11 +0100

    About page: opmaak en styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,97 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react"
+import {graphql} from "gatsby"
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import {
+    header,
+    headerInfo,
+    headerPicture,
+    subtitle,
+    missionSection,
+    missionInfo
+} from "../page.module.css"
 
+const AboutPage = ({
+    data: {
+        wpPage: {
+            aboutPageFields: {
+                headerAboutUs,
+                mission
+            }
+        }
+    }
+}) => {
+    const imageHeader = getImage(headerAboutUs.picture.localFile)
+    const imageMission = getImage(mission.bannerPicture.localFile)
 
-const AboutPage = () => {
     return (
         <Layout pageTitle="About Us">
-            <p>Doggie-dogs is founded since 1998. That is when I was born. My dawgs are the strongest.</p>
+            <div className={header}>
+                <div className={headerInfo}>
+                    <h2 className={subtitle}>
+                        {
+                        headerAboutUs.title
+                    }</h2>
+                    <div dangerouslySetInnerHTML={
+                        {__html: headerAboutUs.description}
+                    }/>
+                </div>
+                <GatsbyImage className={headerPicture}
+                    image={imageHeader}
+                    alt={
+                        headerAboutUs.picture.altText
+                    }/>
+            </div>
+            <div className={missionSection}>
+                <GatsbyImage className={headerPicture}
+                    image={imageMission}
+                    alt={
+                        mission.bannerPicture.altText
+                    }/>
+                <div className={missionInfo}>
+                    <h2 className={subtitle}>
+                        {
+                        mission.title
+                    }</h2>
+                    <div dangerouslySetInnerHTML={
+                        {__html: mission.description}
+                    }/>
+                </div>
+            </div>
         </Layout>
     )
 }
 
+
 export default AboutPage
+
+export const query = graphql `
+  query {
+    wpPage(slug: { eq: "about-us" }) {
+      aboutPageFields {
+        headerAboutUs {
+          title
+          description
+          picture {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(quality: 100)
+              }
+            }
+          }
+        }
+        mission {
+          title
+          description
+          bannerPicture {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(quality: 100)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
