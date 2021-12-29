05dcdda8726f46343bafc2cbb94f213ccbc6a019
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-12-29 14:42:40 +0100

    Artist template page: opmaak en styling

--- src/pages/dogs/{wpDog.slug}.js
+++ src/pages/dogs/{wpDog.slug}.js
@@ -2,79 +2,186 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import {GatsbyImage, getImage} from "gatsby-plugin-image"
 import {graphql} from 'gatsby'
-
+import {
+    header,
+    headerInfo,
+    headerPicture,
+    artistName,
+    artistRoles,
+    artistDescription,
+    artistInfo,
+    artistPictures,
+    artistPicture
+} from "../../page.module.css"
 
 const DogPage = ({
     data: {
         wpDog: {
-            dogFields: dog
+            dogFields: dog,
+            terms: {
+                nodes: terms
+            }
         }
     }
 }) => {
-    const image = getImage(dog.images.picture1.localFile)
+    const image = getImage(dog.dogpicture.localFile)
+    const picture1 = getImage(dog.images.picture1.localFile)
+    const picture2 = getImage(dog.images.picture2.localFile)
+    const picture3 = getImage(dog.images.picture3.localFile)
+
 
     return (
-        <Layout pageTitle="Dogs Template">
-            <div>
-                <GatsbyImage image={image}
+        <Layout pageTitle="Dog Template">
+            <div className={header}>
+                <div className={headerInfo}>
+                    {
+                    dog.name && <h3 className={artistName}>
+                        {
+                        dog.name
+                    }</h3>
+                }
+                    <div className={artistRoles}>
+                        {
+                        terms.map((term, i) => (
+                            <span key={i}>
+                                {
+                                term.name
+                            }
+                                {
+                                i + 1 < terms.length && "- "
+                            } </span>
+                        ))
+                    } </div>
+
+                    <div className={artistDescription}
+                        dangerouslySetInnerHTML={
+                            {__html: dog.description}
+                        }/>
+                    <p className={artistInfo}>
+                        <span>Name:
+                        </span>
+                        {
+                        dog.name
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>Age:
+                        </span>
+                        {
+                        dog.age
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>Sex:
+                        </span>
+                        {
+                        dog.sex
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>Price:
+                        </span>
+                        {
+                        dog.price
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>Wormed:
+                        </span>
+                        {
+                        dog.wormed
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>Sterilized:
+                        </span>
+                        {
+                        dog.sterilized
+                    }</p>
+                    <p className={artistInfo}>
+                        <span>ChieldFriendly:
+                        </span>
+                        {
+                        dog.chieldFriendly
+                    }</p>
+
+                </div>
+                <GatsbyImage className={headerPicture}
+                    image={image}
+                    alt={
+                        dog.dogpicture.altText
+                    }/>
+
+            </div>
+            <div className={artistPictures}>
+                <GatsbyImage className={artistPicture}
+                    image={picture1}
                     alt={
                         dog.images.picture1.altText
                     }/>
-
-                <p>Name: {
-                    dog.name
-                }</p>
-                <p>Age: {
-                    dog.age
-                }</p>
-                <p>Sex: {
-                    dog.sex
-                }</p>
-                <p>Price: € {
-                    dog.price
-                }</p>
-                <p>Wormed: {
-                    dog.wormed
-                }</p>
-                <p>Sterilized: {
-                    dog.sterilized
-                }</p>
-                <p>ChieldFriendly: {
-                    dog.chieldFriendly
-                }</p>
-                <div dangerouslySetInnerHTML={
-                    {__html: dog.description}
-                }/>
+                <GatsbyImage className={artistPicture}
+                    image={picture2}
+                    alt={
+                        dog.images.picture2.altText
+                    }/>
+                <GatsbyImage className={artistPicture}
+                    image={picture3}
+                    alt={
+                        dog.images.picture3.altText
+                    }/>
             </div>
         </Layout>
     )
 }
 
 export const query = graphql `
-  query ($id: String) {
+query ($id: String) {
     wpDog(id: {eq: $id}) {
-        dogFields {
-            name
-            age
-            sex
-            price
-            wormed
-            sterilized
-            chieldFriendly
-            description
-            images {
-                picture1 {
-                  localFile {
-                    childImageSharp {
-                      gatsbyImageData(placeholder: BLURRED, width: 800, height: 500)
-                    }
-                  }
-                  altText
-                }
+      dogFields {
+        name
+        age
+        sex
+        price
+        wormed
+        sterilized
+        chieldFriendly
+        description
+        dogpicture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
             }
+          }
+          altText
         }
+        images {
+          picture1 {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+              }
+            }
+          }
+          picture2 {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+              }
+            }
+          }
+          picture3 {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+              }
+            }
+          }
+        }
+      }
+      terms {
+        nodes {
+          name
+        }
+      }
     }
-
-}`
+  }`
 
 export default DogPage
