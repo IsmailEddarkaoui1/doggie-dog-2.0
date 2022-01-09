c223a3a6a35e7d14bae0b56ef3d129074006d1fa
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2022-01-09 12:19:37 +0100

    Netlify gemaakt

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({children}) => {
     const data = useStaticQuery(graphql `
       query {
         site {
-          siteMetadata {
+          siteMetadata { 
             title
           }
         }
